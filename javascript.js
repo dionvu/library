49224922151d70124be369eaf4d9fdf8f2427879
Dion Vu <dionvu11@gmail.com>
2024-01-27 12:14:21 -0800

    Added ability to add new books using the button.

--- javascript.js
+++ javascript.js
@@ -8,6 +8,7 @@ const acotar1 = new Book("A Court of Mist and Fury", "Sarah J. Maas", 420, false
 // DOM elements
 const bookList = document.getElementById("book-list");
 const header = document.getElementById("header");
+const addButton = document.getElementById("create-book");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,6 +20,8 @@ function Book(title, author, pages, read) {
 Book.prototype.addBookToLibrary = function() {
   library.push(this);
 }
+
+
 function displayBooks(library) {
   for (let i = 0; i < library.length; i++) {
     const box = document.createElement("div");
@@ -43,21 +46,50 @@ function displayBooks(library) {
     box.appendChild(boxPages);
 
     const boxButton = document.createElement("button");
-    boxButton.addEventListener('click', (e) => {
+    boxButton.addEventListener('click', () => {
+
+      const index = library.findIndex(book => book.title == boxTitle.textContent);
+      library.splice(index, 1);
       box.remove();
+
       for (let i = 0; i < library.length; i++) {
-        if (library[i].title === boxTitle.textContent) {
-          library.pop(i);
-        }
+        console.log(library[i]);
       }
     });
 
     boxButton.classList.add('delete-button');
+
     box.appendChild(boxButton);
 
     bookList.appendChild(box);
   }
 }
+
+const dialog = document.querySelector("dialog");
+
+addButton.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  var pages = document.getElementById('pages').value;
+
+  console.log(pages);
+
+  dialog.close();
+  form.reset();
+
+  const newBook = new Book("lol", "hi", pages, false);
+
+  newBook.addBookToLibrary();
+
+  displayBooks(library.slice(library.length - 1, library.length));
+});
+
 
 acotar.addBookToLibrary();
 acotar1.addBookToLibrary();
